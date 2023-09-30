b531657ba8a3e46140e05faa267faa5b47796a33
Elul Asfaha <scorpiondota22@gmail.com>
2023-09-30 17:48:41 +0300

    removed the formik for now will add it back once i go through the docs working on the other parts of the ui

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,30 +1,54 @@
-import { Formik, Form, Field } from "formik";
-type MyFormValues = {
-    firstName: string;
-};
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook } from "react-icons/bs";
+import { SiShopify } from "react-icons/si";
+
 const Signup = () => {
-    const initialValues: MyFormValues = { firstName: "" };
     return (
-        <div>
-            <h1>My Example</h1>
-            <Formik
-                initialValues={initialValues}
-                onSubmit={(values, actions) => {
-                    console.log({ values, actions });
-                    alert(JSON.stringify(values, null, 2));
-                    actions.setSubmitting(false);
-                }}
-            >
-                <Form>
-                    <label htmlFor='firstName'>First Name</label>
-                    <Field
-                        id='firstName'
-                        name='firstName'
-                        placeholder='First Name'
+        <div className='grid grid-cols-2 bg-[#112540] w-[80%] h-[80%] rounded-lg'>
+            <div className='flex flex-col gap-3 text-white p-5'>
+                <p className='text-2xl'>Let's get you Signed Up</p>
+                <p className='text-gray-500'>Start your 30-day free trial</p>
+                <div className='flex gap-4 justify-between'>
+                    <button className='flex-1 flex gap-2 items-center justify-center bg-[#203858] rounded-sm px-5 py-1'>
+                        <FcGoogle />
+                        <span className='hidden lg:flex'>Google</span>
+                    </button>
+                    <button className='flex-1 flex gap-2 items-center justify-center bg-[#203858] rounded-sm px-5 py-1'>
+                        <BsFacebook className='text-blue-500' />
+                        <span className='hidden lg:flex'>Facebook</span>
+                    </button>
+                    <button className='flex-1 flex gap-2 items-center justify-center bg-[#203858] rounded-sm px-5 py-1'>
+                        <SiShopify className='text-green-400' />
+                        <span className='hidden lg:flex'>Shopify</span>
+                    </button>
+                </div>
+            </div>
+            <div className='flex flex-col justify-center px-10'>
+                <div className='flex flex-col gap-2 items-center px-8 bg-[#0B1B32] rounded-lg h-[90%] py-10'>
+                    <p className='text-gray-200'>
+                        We currently use Reaktion across our customers to give
+                        them better instight into their entire buisness.
+                        Reaktion is a tool that i whish had been there years
+                        ago. Thisi means that we now have everything together in
+                        one place, and have the wildest tools for tracking data,
+                        CLTV, and much more. if you are a buisness owner or
+                        agency ownerm Reaktion is a must. Definetly the best
+                        tool on the market at all! in addition to the tool, the
+                        team behind Reaktion is always fresh on new ideas and
+                        insights, which are also quickly implemented, Can
+                        definitely be recommended!"
+                    </p>
+                    <img
+                        src='https://unsplash.com/photos/mEZ3PoFGs_k'
+                        alt=''
+                        className='rounded-full h-[60px] w-[60px]'
                     />
-                    <button type='submit'>Submit</button>
-                </Form>
-            </Formik>
+                    <p className='text-white'>
+                        Emil nissen -{" "}
+                        <span className='text-gray-200'>CEO & Director</span>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
